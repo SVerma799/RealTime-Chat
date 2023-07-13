36091ac8fc7fbfcc2993a291825b78b50d672ba6
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-07-13 14:58:23 -0700

    Changes done to add the Redis DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Button from "./components/ui/Button";
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  await db.set("Hello from the server!", "hello");
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Button variant="default" size="default">
