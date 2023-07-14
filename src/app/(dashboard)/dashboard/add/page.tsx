ef8204cb8d36ea9b408af988ae0e9d570fa76d1b
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2023-07-13 18:54:54 -0700

    Add friend comp

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import AddFriend from "@/components/AddFriend";
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="pt-8">
+      <h1 className="font-bold  text-5xl mb-8">Add a friend</h1>
+      <AddFriend />
+    </main>
+  );
+};
+
+export default Page;
